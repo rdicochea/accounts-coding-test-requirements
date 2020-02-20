20c3c68e3b3866b3262f2627b8b00e9a06ba23c5
Ricardo J Dicochea <ricardo@propelitech.com>
2020-02-20 14:06:38 -0700

    Initial project creation .NET Core 3.1 Web Application MVC

---
+++ accounts-coding-test/accounts-coding-test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace accounts_coding_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
