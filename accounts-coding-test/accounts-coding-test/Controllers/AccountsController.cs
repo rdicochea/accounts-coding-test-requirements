a04cd1631eb8a3a95110c3e413ae6c49fbe3fa65
Ricardo J Dicochea <ricardo@propelitech.com>
2020-02-21 08:06:39 -0700

    Added Accounts View

---
+++ accounts-coding-test/accounts-coding-test/Controllers/AccountsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using accounts_coding_test.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace accounts_coding_test.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<AccountsViewModel> mod = new List<AccountsViewModel>();
+            return View(mod);
+        }
+    }
+}
--- accounts-coding-test/accounts-coding-test/Models/AccountsViewModel.cs
+++ accounts-coding-test/accounts-coding-test/Models/AccountsViewModel.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace accounts_coding_test.Models
 {
-    public class AccountViewModel
+    public class AccountsViewModel
     {
         public int Id { get; set; }
+        [DisplayName("Name")]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        [DisplayName("Phone Number")]
         public string PhoneNumber { get; set; }
-        public float AmountDue { get; set; }
+        [DisplayName("Amount Due")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public double AmountDue { get; set; }
+        [DisplayName("Due Date")]
         public string PaymentDueDate { get; set; }
         public int AccountStatusId { get; set; }
     }
